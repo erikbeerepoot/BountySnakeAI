27a6434063c0050e23949385af35b103ba94a84e
Simon Taft <taft.simon@gmail.com>
2016-02-09 11:51:44 -0800

    Switch to python, add workday branded head

---
+++ app/main.py
@@ -0,0 +1,59 @@
+import bottle
+
+
+@bottle.route('/static/<path:path>')
+def static(path):
+    return bottle.static_file(path, root='static/')
+
+
+@bottle.get('/')
+def index():
+    head_url = '%s://%s/static/head.png' % (
+        bottle.request.urlparts.scheme,
+        bottle.request.urlparts.netloc
+    )
+
+    return {
+        'color': '#3398CC',
+        'head': head_url
+    }
+
+
+@bottle.post('/start')
+def start():
+    data = bottle.request.json
+
+    # TODO: Do things with data
+
+    return {
+        'taunt': 'battlesnake-python!'
+    }
+
+
+@bottle.post('/move')
+def move():
+    data = bottle.request.json
+
+    # TODO: Do things with data
+
+    return {
+        'move': 'north',
+        'taunt': 'battlesnake-python!'
+    }
+
+
+@bottle.post('/end')
+def end():
+    data = bottle.request.json
+
+    # TODO: Do things with data
+
+    return {
+        'taunt': 'battlesnake-python!'
+    }
+
+
+# Expose WSGI app (so gunicorn can find it)
+application = bottle.default_app()
+if __name__ == '__main__':
+    bottle.run(application, host='127.0.0.1', port=8080)
