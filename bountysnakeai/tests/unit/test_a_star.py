e3ee5c57896d5a430446aadc6a60be344c14d50f
Anthony Theocharis <anthony.theocharis@gmail.com>
2016-02-16 13:19:07 -0800

    Update trivial A* test so it passes.
    When two or more nodes have an equal f_score (i.e. H+G), min(openset, key...)
    is nondeterministic, so we check both paths in our test.

--- bountysnakeai/tests/unit/test_a_star.py
+++ bountysnakeai/tests/unit/test_a_star.py
@@ -4,17 +4,35 @@ from bountysnakeai import helper
 class TestAStar(TestCase):
 
     def test_2x2(self):
+        Node = helper.Node
+
         grid = [
             [
-                helper.Node(1,(0,0)),
-                helper.Node(1,(1,0)),
+                Node(1,(0,0)),
+                Node(1,(0,1)),
             ],
             [
-                helper.Node(1,(0,1)),
-                helper.Node(1,(1,1)),
+                Node(1,(1,0)),
+                Node(1,(1,1)),
             ],
         ]
-        goal = helper.Node(1, (0,0))
-        start = helper.Node(1, (1,1))
+
+        start = Node(1, (1,1))
+        goal = Node(1, (0,0))
+
         result = helper.a_star(grid, goal, start)
-        assert False # This test isn't quite right...
+
+        # There are two valid paths to the goal and our algorithm is
+        # non-deterministic, so check both!
+        self.assertIn(result, [
+            [
+                Node(1, (1, 1)),
+                Node(1, (1, 0)),
+                Node(1, (0, 0)),
+            ],
+            [
+                Node(1, (1, 1)),
+                Node(1, (0, 1)),
+                Node(1, (0, 0)),
+            ],
+        ])
