baca90ae81d2fc415af357f41faa10efb19a0f1c
Anthony Theocharis <anthony.theocharis@gmail.com>
2016-02-18 02:33:30 -0800

    Add new test to ensure build_grid() functions correctly.

--- bountysnakeai/tests/unit/test_a_star.py
+++ bountysnakeai/tests/unit/test_a_star.py
@@ -1,5 +1,9 @@
+import json
+
 from bountysnakeai.tests import TestCase
 from bountysnakeai import a_star
+from bountysnakeai import model
+from bountysnakeai.tests.unit import example_json
 
 class TestAStar(TestCase):
 
@@ -30,3 +34,31 @@ class TestAStar(TestCase):
             Node(0, 1),
             Node(1, 1),
         ])
+
+    def test_build_grid(self):
+        # FIXME: Super bad form to something like this in a unit test, but
+        #        it's fastest to code right now...
+        json_dict = json.loads(example_json.dummy_game)
+        bs = model.BoardState(json_dict)
+
+        grid = a_star.build_grid(bs.width, bs.height, bs.snake_list)
+
+        Point = model.Point
+        known_snake_positions = set([
+            Point(1, 1), Point(1, 2), Point(2, 2), # snake 1
+            Point(4, 4), Point(4, 3), Point(4, 2), Point(4, 1) # snake 2
+        ])
+        known_food_positions = set([
+            Point(1, 2), Point(9, 3),
+        ])
+
+        for x in xrange(bs.width):
+            for y in xrange(bs.height):
+                node = grid[x][y]
+                p = Point(x, y)
+                if p in known_snake_positions:
+                    self.assertEqual(node.contents, a_star.SNAKE)
+                elif p in known_food_positions:
+                    self.assertEqual(node.contents, a_star.FOOD)
+                else:
+                    self.assertEqual(node.contents, a_star.EMPTY)
