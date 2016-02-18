a7a5072d533476aae7689c5ba162ef535f0ab741
Anthony Theocharis <anthony.theocharis@gmail.com>
2016-02-18 08:37:53 -0800

    Improve test for the / endpoint to check the response body.

--- bountysnakeai/tests/functional/test_index.py
+++ bountysnakeai/tests/functional/test_index.py
@@ -1,3 +1,5 @@
+import json
+
 from bountysnakeai.tests import TestApp
 from bountysnakeai.tests import TestCase
 from bountysnakeai import main
@@ -7,4 +9,11 @@ class TestIndex(TestCase):
     def test_index(self):
         app = TestApp(main.application)
         response = app.get('/')
-        assert response.status == '200 OK'
+        self.assertEqual(response.status, '200 OK')
+        json_body = json.loads(response.body)
+        self.assertEqual(json_body, {
+            'color': '#3398CC',
+            'head': 'http://localhost:80/static/head.png',
+        })
+        # TODO: Update this test to use a non-default scheme (HTTPS vs HTTP)
+        #       and domain (e.g. not localhost:80)
