1fd2da748f3f14480c194f227ab52adc65f8fe17
erikbeerepoot@workday.com <erik.beerepoot@workday.com>
2016-02-19 08:57:25 -0800

    Added simple algorithm to pick a corner to hide in

--- bountysnakeai/helper.py
+++ bountysnakeai/helper.py
@@ -1,7 +1,7 @@
 from a_star import *
 
 #Prefer lookups over a conditional
-moveLUT =[[u'invalid (0,0)',u'west',u'invalid (0,2)'],[u'north',u'invalid (1,1)', u'south'],[u'invalid (2,0)',u'east',u'invalid (2,2)']]
+moveLUT =[[u'invalid (0,0)',u'west',u'invalid (0,2)'],[u'south',u'invalid (1,1)', u'north'],[u'invalid (2,0)',u'east',u'invalid (2,2)']]
 
 def getSnake(board_state, snake_id):
     for snake in board_state.snake_list:
@@ -27,8 +27,6 @@ def get_food(board_state,snake):
         snakeLocation = snake.coords[0] 
         foodLocation = board_state.food_list[0]
 
-
-
         startNode = Node(snakeLocation[0],snakeLocation[1])
         goalNode = Node(foodLocation[0],foodLocation[1])
 
@@ -43,6 +41,37 @@ def get_food(board_state,snake):
         
         #Turn path into a move we can pass back
         return compute_relative_move(path[1],snakeLocation) 
+
+def pick_corner(board_state,snake):
+        '''
+        Compares the paths to each of the corners, and picks the "best" one
+        '''
+        #0. Set up  target locations 
+        startLocation = Node(snake.coords[0][0],snake.coords[0][1]))
+        top_left = Node(0,0)
+        top_right = Node(0,board_state.width)
+        bottom_right = Node(board_state.height,board_state.width)
+        bottom_left =  Node(board_state.height,0)
+        corners = [top_left,top_right,bottom_right,bottom_left]
+
+        #0b. Build grid
+        grid = build_grid(board_state.width,board_state.height, [], [])
+
+        #1. Plan a path to each corner
+        path_to_tl = find_path(grid,startLocation,top_left)         
+        path_to_tr = find_path(grid,startLocation,top_right)         
+        path_to_br = find_path(grid,startLocation,bottom_right)         
+        path_to_bl = find_path(grid,startLocation,bottom_left)         
+        paths = [path_to_tl,path_to_tr,path_to_br,path_to_bl]
+
+        #2. Compare the cost 
+        cost = [path_to_tl(-1).G,path_to_tl(-1).G,path_to_br(-1).G, path_to_bl(-1).G]
+        indexOfLowestCostPath = cost.index(min(cost))
+        
+        #Debug
+        picked_corner = corners(indexOfLowestCostPath)
+        print("Picked corner: " + picked_corner.x + "," + picked_corner.y)
+        return cost[indexOfLowestCostPath]
 
 def compute_relative_move(move,snakeLocation):
         #Compute the difference (offset by one)
