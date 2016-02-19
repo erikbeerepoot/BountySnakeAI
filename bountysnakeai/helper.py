0b055aac9466fb7aa62190f8f8430e82d7f07a0c
erikbeerepoot@workday.com <erik.beerepoot@workday.com>
2016-02-19 10:04:35 -0800

    Fix another typo

--- bountysnakeai/helper.py
+++ bountysnakeai/helper.py
@@ -70,7 +70,7 @@ def pick_corner(board_state,snake):
         costs.append(path_to_tr[-1] if len(path_to_tr) > 0 else [] )
         costs.append(path_to_br[-1] if len(path_to_br) > 0 else [] )
         costs.append(path_to_bl[-1] if len(path_to_bl) > 0 else [] )
-        if len(costs > 0):
+        if len(costs) > 0:
                 indexOfLowestCostPath = cost.index(min(cost))
         else: 
                 return []
