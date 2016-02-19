41fef2e2debda4c0e817037eb63c04fca4949265
erikbeerepoot@workday.com <erik.beerepoot@workday.com>
2016-02-18 17:12:43 -0800

    Starting to use a* to plan toward food (very simplistic)

--- bountysnakeai/helper.py
+++ bountysnakeai/helper.py
@@ -10,3 +10,13 @@ def health_threshold(board_state):
     """
     # XXX: This is a very naive heuristic.
     return (board_state.width * board_state.height) // 4
+
+def get_food(board_state,snake):
+        '''
+        Return the next best move to the food
+        '''
+        
+        grid = build_grid(board_state.width, board_state.height, [], board_state.food_list)
+        path = find_path(grid,snake.coords[0],board_state.food_list[0])
+        return path[0]
+
