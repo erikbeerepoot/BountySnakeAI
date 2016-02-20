373410ad1ba46eac655dd5f96437091cdb1fd93e
Anthony Theocharis <anthony.theocharis@gmail.com>
2016-02-20 02:52:03 -0800

    Update log format.

--- bountysnakeai/__init__.py
+++ bountysnakeai/__init__.py
@@ -6,6 +6,6 @@ log_level = logging.DEBUG
 log.setLevel(log_level)
 stdout_handler = logging.StreamHandler(sys.stdout)
 stdout_handler.setLevel(log_level)
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+formatter = logging.Formatter('%(asctime)s - %(name)-20s - %(levelname)7s - %(message)s')
 stdout_handler.setFormatter(formatter)
 log.addHandler(stdout_handler)
