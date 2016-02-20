011cae2c458d5ba23776828be2e5cd3e5c4e886e
Erik Beerepoot <erikbeerepoot@hush.com>
2016-02-20 07:15:32 -0800

    Added simple test for taunting

---
+++ bountysnakeai/tests/unit/test_helper.py
@@ -0,0 +1,18 @@
+import json
+
+from bountysnakeai.tests import TestCase
+from bountysnakeai import model
+from bountysnakeai.tests.unit import example_json
+from bountysnakeai import helper
+
+class TestHelper(TestCase):
+
+    def test_taunts(self):
+	json_dict = json.loads(example_json.dummy_game)
+        bs = model.BoardState(json_dict)
+
+	taunt = helper.taunt_opponent(bs)
+	print(taunt)
+	assert(len(taunt)>0)
+
+
