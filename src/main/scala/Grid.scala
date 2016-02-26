d2363d0a210afbb3c771cbb6704e56d439cb7ece
Erik Beerepoot <erikbeerepoot@hush.com>
2016-02-26 12:26:11 -0800

    Cleaning up Grid class by moving to parameterized grid

--- src/main/scala/Grid.scala
+++ src/main/scala/Grid.scala
@@ -1,30 +1,60 @@
-sealed class GridValue(val cost : Int = 0, val name : String = "")
-case object Self extends GridValue {override val name = "self"; override val cost = 100 }
-case object Enemy extends GridValue {override val name = "enemy"; override val cost = 100 }
-case object Food extends GridValue {override val name = "food"; override val cost = -10 }
-case object Gold extends GridValue {override val name = "gold"; override val cost = -100}
-case object Wall extends GridValue {override val name = "wall"; override val cost = 100 }
-case object Path extends GridValue {override val name = "path"; override val cost = 0 }
-case object Infinity extends GridValue {override val name = "infinity"; override val cost = Int.MaxValue}
+import scala.reflect.ClassTag
 
-case class Grid(val width : Int,val height : Int) {
-  var values : Array[Array[GridValue]] = Array.ofDim[GridValue](width,height)
+sealed class GridValue(val cost : Double = 0, val name : String = ""){
+  override def toString() = cost.toString
+}
+case object Self extends GridValue {override val name = "self"; override val cost = 100.0 }
+case object Enemy extends GridValue {override val name = "enemy"; override val cost = 100.0 }
+case object Food extends GridValue {override val name = "food"; override val cost = -10.0 }
+case object Gold extends GridValue {override val name = "gold"; override val cost = -100.0}
+case object Wall extends GridValue {override val name = "wall"; override val cost = 100.0 }
+case object Infinity extends GridValue {override val name = "infinity"; override val cost = Double.MaxValue}
 
-  def printBoard() = {
-    values foreach { row => row foreach print; println }
+case object Path extends GridValue {
+    override val name = "path"; 
+    override val cost = 0.0
+    override def toString() = "P"
+}
+
+case object Start extends GridValue { 
+    override val name = "start" 
+    override val cost = 0.0
+    override def toString() = "S"
+}
+
+case object Goal extends GridValue { 
+    override val name = "goal"; 
+    override val cost = 0.0
+    override def toString() = "G"
+}
+
+case class Grid[A : ClassTag](val width : Int, val height : Int){
+  var values : Array[Array[A]] = Array.ofDim[A](width,height)
+ 
+  def apply(point : Point) = {
+    values(point.x)(point.y)
   }
 
-  def addPoints(points : List[Point], value : GridValue) = {
+  def update(point : Point, value : A) = {
+    values(point.x)(point.y) = value
+  }
+
+  def printGrid() = {
+    values foreach { row => row foreach { value => value match {
+      case value : GridValue => System.out.printf("%4d",value) 
+      case point : Point => print(f"(${point.x},${point.y}),")  
+      case _ => 
+      }
+    }; println;
+   }
+  }
+  def addPoints(points : List[Point], value : A) = {
     points foreach { point => values(point.x)(point.y) = value }
   }
 
-  def apply(node : Node) = values(node.point.x)(node.point.y)
-  def apply(point : Point) = values(point.x)(point.y)
-
-  def update(node : Node, value : Int) = values(node.point.x)(node.point.y) = new GridValue(value)
-  def update(point: Point, value : Int) = values(point.x)(point.y) = new GridValue(value)
-
-  def setGridToValue(value : GridValue) = {
+  def setGridToValue(value : A){
     values = Array.tabulate(width,height)((x,y) => value)
   }
 }
+
+
