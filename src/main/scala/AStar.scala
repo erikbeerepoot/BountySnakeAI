11715ba53d3bcfbb7a5a06b962a333990ea89bae
Erik Beerepoot <erikbeerepoot@hush.com>
2016-02-26 12:25:15 -0800

    Cleanup a* by moving to use parameterized grid class

--- src/main/scala/AStar.scala
+++ src/main/scala/AStar.scala
@@ -2,39 +2,40 @@ import scala.collection.mutable.PriorityQueue
 import scala.collection.mutable.ListBuffer
 import scala.math.Ordering.Implicits._
 
-case class Node(val point : Point, val priority : Int) extends Ordered[Node] {
-  def compare(node : Node) = this.priority.compareTo(node.priority)
+case class Node(val point : Point, val priority : Double = 0) extends Ordered[Node] {
+  def compare(node : Node) = node.priority.compareTo(this.priority)
 }
 
 
-class AStar(var grid : Grid){
+class AStar(var grid : Grid[Double]){
 
     var closedSet = PriorityQueue[Node]()
     var openSet = PriorityQueue[Node]()
 
     def buildGrid(snakes : List[Snake], food : List[Point], walls : List[Point], gold : List[Point]){
-      snakes foreach { snake => grid.addPoints(snake.coords,Enemy)}
-      grid.addPoints(food,Food)
-      grid.addPoints(walls,Wall)
-      grid.addPoints(gold,Gold)
+      snakes foreach { snake => grid.addPoints(snake.coords,Enemy.cost)}
+      grid.addPoints(food,Food.cost)
+      grid.addPoints(walls,Wall.cost)
+      grid.addPoints(gold,Gold.cost)
     } 
 
-    def planPath(start : Node, goal : Node){
+    def planPath(start : Point, goal : Point) : List[Point] = {
         var closedSet = PriorityQueue[Node]()
-        var openSet = PriorityQueue[Node](start)
+        var openSet = PriorityQueue[Node](Node(start,0))
         
-        var originGrid = Grid(grid.width,grid.height)
-        var g_score = Grid(grid.width,grid.height)
-        var f_score = Grid(grid.width,grid.height)
-        g_score.setGridToValue(Infinity)
-        f_score.setGridToValue(Infinity)
-        g_score(start) = 0
+        var originGrid = Grid[Point](grid.width,grid.height)
+        var g_score = Grid[Double](grid.width,grid.height)
+        var f_score = Grid[Double](grid.width,grid.height)
+        g_score.setGridToValue(Infinity.cost)
+        f_score.setGridToValue(Infinity.cost)
+        g_score(start) = 0 
         f_score(start) = estimateCost(start,goal)
 
         while(!openSet.isEmpty){
           val current = openSet.dequeue()
-          if(current == goal){
-            return reconstructPath(originGrid,goal)
+          if(current.point == goal){
+            g_score.printGrid()
+            return reconstructPath(originGrid,start,goal)
           }
 
           //Add the current node to the list of evaluated nodes
@@ -43,13 +44,18 @@ class AStar(var grid : Grid){
           //for each neighbour of current
           neighboursForNode(current) foreach { neighbour =>
             if(!pointInQueue(neighbour,closedSet)){
-              val tentativeScore = g_score(current).cost + manhattanDistance(current.point,neighbour)
+              val tentativeScore = g_score(current.point) + euclidianDistance(current.point,neighbour)
               
-              //Discover a new node 
-              if(!pointInQueue(neighbour,openSet) || tentativeScore <= g_score(neighbour).cost){
-                originGrid(neighbour) = 1 
+              //We've discovered a more efficient path 
+              if(!pointInQueue(neighbour,openSet)){
+                //discover a new node 
+                val priority = tentativeScore + estimateCost(neighbour,goal)
+                openSet.enqueue(Node(neighbour,priority))
+              }
+              if(tentativeScore < g_score(neighbour)){
                 g_score(neighbour) = tentativeScore
-                f_score(neighbour) = g_score(neighbour).cost + estimateCost(neighbour,goal.point)
+                f_score(neighbour) = g_score(neighbour) + estimateCost(neighbour,goal)
+                originGrid(neighbour) = current.point
               } 
             }
           }
@@ -57,7 +63,14 @@ class AStar(var grid : Grid){
         return List[Point]()
     }
 
-    def reconstructPath(originGrid : Grid, goal : Node) : List[Point] = {
+    def reconstructPath(originGrid : Grid[Point] , start : Point, goal : Point) : List[Point] = {
+      var path = ListBuffer[Point]()
+      var currentPoint = goal
+      while(currentPoint != start){
+        path += currentPoint
+        currentPoint = originGrid(currentPoint)
+      }
+      return path.toList    
     }
 
     def pointInQueue(point : Point, queue : PriorityQueue[Node]) = {
@@ -67,12 +80,21 @@ class AStar(var grid : Grid){
       }
     }
 
-    def estimateCost(start : Node, goal : Node) : Int = {
-       manhattanDistance(start,goal) 
+    def estimateCost(start : Node, goal : Node) : Double = {
+       return manhattanDistance(start,goal) 
+       val pointA = start.point
+       val pointB = goal.point
+       return euclidianDistance(pointA,pointB)
     }
 
-    def estimateCost(start : Point, goal : Point) : Int = {
-       manhattanDistance(start,goal) 
+    def estimateCost(start : Point, goal : Point) : Double = {
+       return manhattanDistance(start,goal) 
+       //return euclidianDistance(start,goal)
+    }
+
+    def euclidianDistance(pointA : Point, pointB : Point) : Double = {
+       return math.sqrt(math.abs(pointA.y - pointB.y)^2 + math.abs(pointA.x - pointB.x)^2)
+      
     }
 
     def manhattanDistance(nodeA : Node, nodeB : Node) : Int = math.abs(nodeA.point.x - nodeB.point.x) + math.abs(nodeA.point.y - nodeB.point.y)
