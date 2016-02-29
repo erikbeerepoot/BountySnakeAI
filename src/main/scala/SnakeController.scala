0ea1cbd9fdf3616edaeac6e3c92605f1e9ebd030
Erik Beerepoot <erikbeerepoot@hush.com>
2016-02-28 17:37:13 -0800

    Updated planning code to extract current position and plan towards the food

--- src/main/scala/SnakeController.scala
+++ src/main/scala/SnakeController.scala
@@ -2,18 +2,33 @@ package com.barefoot.bountysnake
 import PointJsonProtocol._
 
 case class SnakeController(var gameState : Game){
+  val snakeId = "1234-567890-123456-7890"
   var currentPoint : Point = Point(0,0)
   var path : List[Point] = List[Point]()
   var costGrid : Grid[Double] = Grid[Double](gameState.width,gameState.height)
 
-
+  //update the state of the game from our model
   def updateState(gameState : Game){
     this.gameState = gameState
 
-    costGrid = Grid[Double](gameState.width,gameState.height)
-    var planner : AStar = new AStar(costGrid)
+    //extract our position
+    val ourSnake = gameState.snakes.filter(snake => snake.id == snakeId) 
+    val headPosition = ourSnake.coords.headOption match {
+      case Some(head) => {
+        //Create a new planner and board
+        val enemies = gameState.snakes.filter(snake=>snake.id != snakeID)
+        var planner : AStar = new AStar(costGrid)
+        planner.buildGrid(enemies,gameState.food,gameState.walls,gameState.gold)
 
-    path = planner.planPath(Point(1,1),Point(8,8))
+        //If there is food, go towards it
+        if(!food.isEmpty){
+          path = planner.planPath(head,food.head)
+        } else {
+          path = planner.planPath(head,Point(10,10))
+        }
+      }
+      case None => println("Current position unkown. Can't run a*") //TODO: Throw exception
+    }
   }
 
   def getNextMove() : String = {
@@ -35,7 +50,6 @@ case class SnakeController(var gameState : Game){
     } else {
       return "invalid" //TODO: Throw exception
     }
-
   }
     
 }
