bf3149465ba9ee2c01ced49a426f36153aa4beb0
Erik Beerepoot <erikbeerepoot@hush.com>
2016-02-28 17:47:16 -0800

    A bunch of fixes to the SnakeController class

--- src/main/scala/SnakeController.scala
+++ src/main/scala/SnakeController.scala
@@ -3,7 +3,7 @@ import PointJsonProtocol._
 
 case class SnakeController(var gameState : Game){
   val snakeId = "1234-567890-123456-7890"
-  var currentPoint : Point = Point(0,0)
+  var currentPosition : Point = Point(0,0)
   var path : List[Point] = List[Point]()
   var costGrid : Grid[Double] = Grid[Double](gameState.width,gameState.height)
 
@@ -13,16 +13,23 @@ case class SnakeController(var gameState : Game){
 
     //extract our position
     val ourSnake = gameState.snakes.filter(snake => snake.id == snakeId) 
-    val headPosition = ourSnake.coords.headOption match {
+    if(ourSnake.length != 1){
+      //imposters or missing snakes
+      println("Couldn't find our snake :( can't run a*"); //TODO: Throw exception
+      return 
+    }
+    currentPosition = ourSnake.head.coords.head
+
+    val headPosition = ourSnake.head.coords.headOption match {
       case Some(head) => {
         //Create a new planner and board
-        val enemies = gameState.snakes.filter(snake=>snake.id != snakeID)
+        val enemies = gameState.snakes.filter(snake=>snake.id != snakeId)
         var planner : AStar = new AStar(costGrid)
         planner.buildGrid(enemies,gameState.food,gameState.walls,gameState.gold)
 
         //If there is food, go towards it
-        if(!food.isEmpty){
-          path = planner.planPath(head,food.head)
+        if(!gameState.food.isEmpty){
+          path = planner.planPath(head,gameState.food.head)
         } else {
           path = planner.planPath(head,Point(10,10))
         }
@@ -41,15 +48,14 @@ case class SnakeController(var gameState : Game){
     val deltaY = currentPosition.x - path.head.y
     if(deltaX==1){
       return "east"
-    } else if (deltaX==-1){
+    } else if (deltaX == -1){
       return "west"
-    } else if (deltaY==1){
+    } else if (deltaY == 1){
       return "north"
-    } else (deltaY==-1){
+    } else if(deltaY == -1){
       return "south"
     } else {
       return "invalid" //TODO: Throw exception
     }
   }
-    
 }
